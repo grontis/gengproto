8aadc4e98d53db5cee2138a0d029c55f3c398423
Grant Kveton <grontisio314@gmail.com>
2024-08-10 15:29:52 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CMake 
+
+issues with cmake on windows solved by renaming cmake file https://stackoverflow.com/questions/71199549/cmake-in-vsc-doesnt-find-generator
---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+class GVector {
+private:
+
+public:
+    void Draw();
+};
+
+int main() {
+    //TODO world space
+        //2D grid space first
+        //2D library that was used in tetris example engine
+        //Then 3D next
+
+
+    //TODO vector stuff
+        //Vector class
+        //Store vectors
+        //Modify/compute vectors
+        //Draw vectors
+
+    
+    return 0;
+}
