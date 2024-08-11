368c7ec3fcaaa8b353f212b4ea8df4b644953052
grontis <grontisio314@gmail.com>
2024-08-10 23:20:04 -0600

    Initial SDL hello world

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # CMake 
 
 issues with cmake on windows solved by renaming cmake file https://stackoverflow.com/questions/71199549/cmake-in-vsc-doesnt-find-generator
+
+# Dependencies
+## Linux
+```sudo apt-get install libsdl2-dev```
+
+## Windows
+download using vcpkg
--- main.cpp
+++ main.cpp
@@ -1,12 +1,60 @@
 #include <iostream>
 #include <vector>
 
-class GVector {
-private:
+#include <SDL2/SDL.h>
 
+class GVector2D {
+private:
+    std::vector<int> vector;
 public:
+    GVector2D(int x, int y);
+    ~GVector2D();
     void Draw();
+    int Magnitude();
 };
+
+int SDLHelloWorld() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("gengproto", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == nullptr) {
+        SDL_DestroyWindow(window);
+        std::cerr << "SDL_CreaterRenderer Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_Rect rect;
+    rect.x = 200;
+    rect.y = 150;
+    rect.w = 200;
+    rect.h = 100;
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_RenderFillRect(renderer, &rect);
+
+    SDL_RenderPresent(renderer);
+    SDL_Delay(5000);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    
+    return 0;
+}
 
 int main() {
     //TODO world space
@@ -21,6 +69,9 @@ int main() {
         //Modify/compute vectors
         //Draw vectors
 
-    
-    return 0;
+
+    int result = SDLHelloWorld();
+    return result;
+
+    //return 0;
 }
