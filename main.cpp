5b5abef9f350065118b63dde81adb41812557185
grontis <grontisio314@gmail.com>
2024-08-14 00:51:36 -0600

    Initial input handling to move something using WASD and mouse input

--- main.cpp
+++ main.cpp
@@ -3,34 +3,31 @@
 
 #include <SDL2/SDL.h>
 
-class GVector2D {
+class GVector {
 private:
     std::vector<int> vector;
 public:
-    GVector2D(int x, int y);
-    ~GVector2D();
+    GVector(int x, int y);
+    GVector(int x, int y, int z);
+    ~GVector();
     void Draw();
     int Magnitude();
 };
 
-int SDLHelloWorld() {
-
-}
-
 int main() {
-    bool isRunning = true;
-
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    SDL_Window *window = SDL_CreateWindow("gengproto", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Window *window = SDL_CreateWindow("gengproto", 100, 100, 1024, 860, SDL_WINDOW_SHOWN);
     if (window == nullptr) {
         std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
+
+    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (renderer == nullptr) {
@@ -40,44 +37,68 @@ int main() {
         return 1;
     }
 
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderClear(renderer);
+    SDL_Rect mainRect;
+    mainRect.x = 100;
+    mainRect.y = 100;
+    mainRect.w = 50;
+    mainRect.h = 50;
 
-    SDL_Rect rect;
-    rect.x = 200;
-    rect.y = 150;
-    rect.w = 200;
-    rect.h = 100;
+    std::vector<SDL_Rect> rectangles;
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-    SDL_RenderFillRect(renderer, &rect);
-    SDL_RenderPresent(renderer);
+    bool quit = false;
+    SDL_Event event;
 
-    SDL_Event e;
-    while(isRunning) {
-        while ( SDL_PollEvent(&e) != 0) {
-            switch (e.type) {
-                case SDL_QUIT:
-                    return false;
-                case SDL_KEYDOWN:
-                        SDL_RenderClear(renderer);
-                        SDL_Rect rect;
-                        rect.x = 250;
-                        rect.y = 200;
-                        rect.w = 100;
-                        rect.h = 100;
-                        SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
-                        SDL_RenderFillRect(renderer, &rect);
-                        SDL_RenderPresent(renderer);
-                default:
-                    break;
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+            if (event.type == SDL_KEYDOWN) {
+                switch (event.key.keysym.sym) {
+                    case SDLK_w:
+                        mainRect.y -= 10;
+                        break;
+                    case SDLK_s:
+                        mainRect.y += 10;
+                        break;
+                    case SDLK_a:
+                        mainRect.x -= 10;
+                        break;
+                    case SDLK_d:
+                        mainRect.x += 10;
+                        break;
+                }
+            }
+            if (event.type == SDL_MOUSEBUTTONDOWN) {
+                if (event.button.button == SDL_BUTTON_LEFT) {
+                    int x, y;
+                    SDL_GetMouseState(&x, &y);
+                    SDL_Rect newRect;
+                    newRect.x = x;
+                    newRect.y = y;
+                    newRect.w = 10;
+                    newRect.h = 10;
+
+                    rectangles.push_back(newRect);
+                }
             }
         }
 
-        SDL_Delay(100);
-    }
+        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        SDL_RenderClear(renderer);
 
-    SDL_Delay(5000);
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+        SDL_RenderFillRect(renderer, &mainRect);
+
+        for (const auto &rect : rectangles) {
+            SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+            SDL_RenderFillRect(renderer, &rect);
+        }
+
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(16);
+    }
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
